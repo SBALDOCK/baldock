32e78c3d90c0993100946700b48e8cb9d58b739b
SBALDOCK <stephenbaldock78@gmail.com>
2020-12-24 09:49:37 -0800

    formatting background image

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,22 +1,14 @@
-import React from 'react'
-import './Hero.css';
-import '../../App.css';
-import { Button } from '../Button/Button'
+import React from "react";
+import "./Hero.css";
+import "../../App.css";
+import { Button } from "../Button/Button";
 
 function Hero() {
   return (
-    <div className='hero-container'>
-      <h1 className='hero-tag'>Greetings!</h1> 
-      <div className='hero-btns'>
-        {/* <Button className='btns' buttonStyle='btn--outline' buttonSize='btn--large'>
-          Reach Out
-        </Button>
-        <Button className='btns' buttonStyle='btn--primary' buttonSize='btn--large'>
-          Watch Trailer <i className='far fa-play-circle' />
-        </Button> */}
-      </div>
+    <div className="hero-container">
+      <h1 className="hero-tag">Greetings!</h1>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
