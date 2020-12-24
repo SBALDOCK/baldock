fe263cdc965b0f9b3608781684bc5c958aec57fe
SBALDOCK <stephenbaldock78@gmail.com>
2020-12-23 20:18:29 -0800

    mobile formatting

--- src/components/Bio/Bio.js
+++ src/components/Bio/Bio.js
@@ -5,17 +5,17 @@ function Bio() {
   return (
     <div className="bio">
       <p className="about_me">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna
-        tellus, luctus sit amet leo id, malesuada sollicitudin mauris.
-        Pellentesque augue dui, consectetur ut quam in, malesuada molestie
-        neque. Orci varius natoque penatibus et magnis dis parturient montes,
-        nascetur ridiculus mus. Aenean libero metus, gravida quis velit
-        sagittis, faucibus euismod orci. Fusce id ullamcorper dui. Aliquam vitae
-        purus placerat, congue arcu eu, ultrices mauris. Integer quis lectus
-        vitae nunc viverra luctus eget vel leo. Fusce quis pretium ante.
-        Phasellus purus purus, vehicula in tortor et, malesuada condimentum
-        mauris. Vivamus ultrices purus quis egestas pretium. Mauris at faucibus
-        velit.
+        Hi there. My name is Steve and I'm a software developer. I chose to
+        pursue software development because I love to solve problems and I love
+        to collaborate. This industry provides ample opportunity for both.
+        <br /> My background is in music. Yes, I've had standard careers in the
+        telecommunications industry, in private events, and in public education.
+        But my most valuable qualities are those that I've learned while
+        studying music, listening to music, and performing music with others.
+        Music teaches me how to think creatively and out of the box. It teaches
+        me how to collaborate toward a common goal. It also teaches me about how
+        simple solutions are usually the best solutions. Muck like music,
+        software development is a language that I'm excited to keep learning.
       </p>
       <p className="about_me">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer magna
